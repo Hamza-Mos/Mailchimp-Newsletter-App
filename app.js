bc173bbc2fc0ab9c14b94524c8aa813371f0d942
Hamza Mostafa <hamza.oruba@gmail.com>
2022-01-13 10:37:48 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const request = require('superagent');
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port 3000.");
+app.listen(process.env.PORT, function() {
+  console.log("Server is running on the port.");
 });
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => 
+{
   res.sendFile(__dirname + "/signup.html");
 });
-var mailchimpInstance   = 'us5',
-listUniqueId        = 'dfd0daf722',
-mailchimpApiKey     = '393ecd3231fba7d08cc7d8a4ddeb4549-us5';
+var mailchimpInstance   = process.env.MAILCHIMP_INSTANCE,
+listUniqueId        = process.env.LIST_UNIQUE_ID,
+mailchimpApiKey     = process.env.MAILCHIMP_API_KEY;
 
-app.post('/', function (req, res) {
+app.post('/', (req, res) => 
+{
 
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
@@ -27,7 +30,8 @@ request
     .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
     .set('Content-Type', 'application/json;charset=utf-8')
     .set('Authorization', 'Basic ' + Buffer.from('any:' + mailchimpApiKey ).toString('base64'))
-    .send({
+    .send
+    ({
       'email_address': email,
       'status': 'subscribed',
       'merge_fields': {
@@ -35,7 +39,8 @@ request
         'LNAME': lastName
       }
     })
-        .end(function(err, response) {
+        .end((err, response) => 
+        {
           if (response.status < 300) {
             res.sendFile(__dirname + "/success.html");
           } else {
@@ -44,6 +49,7 @@ request
       });
 });
 
-app.post("/failure", function(req, res){
+app.post("/failure", (req, res) => 
+{
   res.redirect("/");
 })
